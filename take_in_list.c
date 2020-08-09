255fefbfa924866a51040edd2104b743fc2769b0
Dende Marsella <dmarsell@mi-r5.msk.21-school.ru>
2020-08-09 18:49:03 +0300

    refact 0.2

--- take_in_list.c
+++ take_in_list.c
@@ -6,7 +6,7 @@
 /*   By: dmarsell <dmarsell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/03 01:03:54 by dmarsell          #+#    #+#             */
-/*   Updated: 2020/08/09 18:41:13 by dmarsell         ###   ########.fr       */
+/*   Updated: 2020/08/09 18:47:50 by dmarsell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,9 +90,6 @@ void			ft_prestart(t_head *head, char **argv, t_crutch *data, t_point *point)
 		point->p_arg_file->next = NULL;
 	if (dop.inval_argp)
 		point->p_inval_argp->next = NULL;
-	// dop.arg_dir ? point->p_arg_dir->next = NULL : 1;
-	// dop.arg_file ? point->p_arg_file->next = NULL : 1;
-	// dop.inval_argp ? point->p_inval_argp->next = NULL : 1;
 }
 
 void			while_arg_dir(t_list *arg_dir, t_crutch data)
@@ -103,6 +100,25 @@ void			while_arg_dir(t_list *arg_dir, t_crutch data)
 		arg_dir->next ? write(1, "\n", 1) : 1;
 		arg_dir = arg_dir->next;
 	}
+}
+
+void	preprestart(t_head *head, char **argv, t_crutch data, t_point *point)
+{
+	ft_prestart(head, argv, &data, point);
+	point->p_inval_argp = sorting(head->invalid_start, data.flags);
+	while(point->p_inval_argp)
+	{
+		print(point->p_inval_argp, data.flags);
+		point->p_inval_argp = point->p_inval_argp->next;
+	}
+	ft_free(head->invalid_start);
+	point->p_arg_file = sorting(head->val_file_start, data.flags);
+		print(point->p_arg_file, data.flags);
+	head->val_dir_start && point->p_arg_file ? write(1, "\n", 1) : 1;
+	ft_free(head->val_file_start);
+	point->p_arg_dir = sorting(head->val_dir_start, data.flags);
+	while_arg_dir(point->p_arg_dir, data);
+	ft_free(head->val_dir_start);
 }
 
 int				main(int argc, char **argv)
@@ -125,20 +141,21 @@ int				main(int argc, char **argv)
 		ft_start(data.flags, data.way);
 		return (0);
 	}
-	ft_prestart(&head, argv, &data, &point);
-	point.p_inval_argp = sorting(head.invalid_start, data.flags);
-	while(point.p_inval_argp)
-	{
-		print(point.p_inval_argp, data.flags);
-		point.p_inval_argp = point.p_inval_argp->next;
-	}
-	ft_free(head.invalid_start);
-	point.p_arg_file = sorting(head.val_file_start, data.flags);
-		print(point.p_arg_file, data.flags);
-	head.val_dir_start && point.p_arg_file ? write(1, "\n", 1) : 1;
-	ft_free(head.val_file_start);
-	point.p_arg_dir = sorting(head.val_dir_start, data.flags);
-	while_arg_dir(point.p_arg_dir, data);
-	ft_free(head.val_dir_start);
+	preprestart(&head, argv, data, &point);
+	// ft_prestart(&head, argv, &data, &point);
+	// point.p_inval_argp = sorting(head.invalid_start, data.flags);
+	// while(point.p_inval_argp)
+	// {
+	// 	print(point.p_inval_argp, data.flags);
+	// 	point.p_inval_argp = point.p_inval_argp->next;
+	// }
+	// ft_free(head.invalid_start);
+	// point.p_arg_file = sorting(head.val_file_start, data.flags);
+	// 	print(point.p_arg_file, data.flags);
+	// head.val_dir_start && point.p_arg_file ? write(1, "\n", 1) : 1;
+	// ft_free(head.val_file_start);
+	// point.p_arg_dir = sorting(head.val_dir_start, data.flags);
+	// while_arg_dir(point.p_arg_dir, data);
+	// ft_free(head.val_dir_start);
 	return (0);
 }
